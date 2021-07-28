25e47ff48a41e70b1ae61c23efe5e7fd61838174
Ana Thaís Rocha de Freitas <anathais@inatel.br>
2021-07-28 10:20:36 -0300

    corrigindo region

--- bin/ecommerce2_cdk.ts
+++ bin/ecommerce2_cdk.ts
@@ -2,20 +2,18 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { Ecommerce2CdkStack } from '../lib/ecommerce2_cdk-stack';
+import { PipelineStack } from '../lib/pipeline/pipeline-stack';
 
 const app = new cdk.App();
-new Ecommerce2CdkStack(app, 'Ecommerce2CdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new PipelineStack(app, "PipelineStack", {
+  env: {
+    account: "962173486310",
+    region: "us-east-1",
+  },
+  tags: {
+    ["const"]:  "ECommerce",
+    ["team"]: "AnaThais",
+  },
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+app.synth();
---
+++ lib/pipeline/pipeline-stack.ts
@@ -0,0 +1,39 @@
+import * as codepipeline from "@aws-cdk/aws-codepipeline";
+import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
+import * as cdk from "@aws-cdk/core";
+import { TreeCloudArtifact } from "@aws-cdk/cx-api";
+import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+
+
+export class PipelineStack extends cdk.Stack{
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps){
+        super(scope, id, props);
+
+        const sourceArtifact = new codepipeline.Artifact();
+        const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+        const pipeline = new CdkPipeline(this, "Pipeline", {
+            pipelineName: "ECommercePipeline",
+            cloudAssemblyArtifact,
+
+
+            sourceAction: new codepipeline_actions.GitHubSourceAction({
+                actionName: "GitHub",
+                output: sourceArtifact,
+                oauthToken: cdk.SecretValue.secretsManager("github-token"),
+                owner: "AnaThais",
+                repo: "ECommerce2CDK",
+                branch: "master",
+            }),
+            synthAction: SimpleSynthAction.standardNpmSynth({
+                sourceArtifact,
+                cloudAssemblyArtifact,
+                installCommand: "npx npm@6 install && npm install -g typescript && npm install -g aws-cdk",
+                buildCommand: "npm run build",
+                environment:{
+                    privileged: true,
+                },
+            }),
+        });
+    }
+}
