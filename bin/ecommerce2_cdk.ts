7ccbbaef96a4a1c0bfe23e17b9e8e6266186577d
Ana Thaís Rocha de Freitas <anathais@inatel.br>
2021-07-29 10:49:44 -0300

    teste

--- bin/ecommerce2_cdk.ts
+++ bin/ecommerce2_cdk.ts
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-//import { Ecommerce2CdkStack } from '../lib/ecommerce2_cdk-stack';
-import { PipelineStack } from '../lib/pipeline/pipeline-stack';
-import { ECommerceStage } from '../lib/pipeline/ecommerce-stage';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { PipelineStack } from "../lib/pipeline/pipeline-stack";
 
 const app = new cdk.App();
 new PipelineStack(app, "PipelineStack", {
