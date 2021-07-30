93316938b34368dd2c9b60135151743b645eefbb
Ana Thaís Rocha de Freitas <anathais@inatel.br>
2021-07-30 11:06:52 -0300

    teste

--- lib/stacks/productDdb-stack.ts
+++ lib/stacks/productDdb-stack.ts
@@ -18,5 +18,25 @@ export class ProductsDdbStack extends cdk.Stack {
       readCapacity: 1,
       writeCapacity: 1,
     });
+
+    const readScale = this.table.autoScaleReadCapacity({
+      maxCapacity: 4,
+      minCapacity: 1,
+   });
+   readScale.scaleOnUtilization({
+      targetUtilizationPercent: 50,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60),
+   });
+
+   const writeScale = this.table.autoScaleWriteCapacity({
+    maxCapacity: 4,
+    minCapacity: 1,
+ });
+ writeScale.scaleOnUtilization({
+    targetUtilizationPercent: 50,
+    scaleInCooldown: cdk.Duration.seconds(60),
+    scaleOutCooldown: cdk.Duration.seconds(60),
+ });
   }
 }
